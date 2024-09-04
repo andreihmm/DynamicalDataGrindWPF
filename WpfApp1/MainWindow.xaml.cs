fb540c8942d81f656d87d5e933102a89d610873c
Andrei Alves Fracalossi <124471216+andreihmm@users.noreply.github.com>
2024-09-04 08:33:26 -0300

    Add files via upload

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BindingList<User> ListaUsuarios = [];
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataGridUser.ItemsSource = ListaUsuarios;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ListaUsuarios.Add(
+                new User()
+                {
+                    Nome = TextBoxNome.Text,
+                    Sobrenome = TextBoxSobrenome.Text,
+                    idade = Convert.ToInt16(TextBoxIdade.Text)
+                }
+            );
+            
+        }
+    }
+}
---
+++ WpfApp1/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class User
+    {
+        public string Nome { get; set; } = "";
+        public string Sobrenome { get; set; } = "";
+        public int idade { get; set; } = 0;
+    }
+}
